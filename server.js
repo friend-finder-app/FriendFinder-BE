b561271285b72876f20c6488446d876fec7f1158
Rod <rod.bromberg@yahoo.com>
2019-07-01 14:22:28 -0400

    rod-bromberg-register

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const users = require("./api/users");
@@ -11,10 +12,22 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log({ err, error: "Couldnt Connect to Mongo DB" }));
+
 server.get("/", (req, res) => {
   res.status(200).json("I'm Alive");
 });
 
 server.use("/api/users", users);
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => console.log(`Port ${port} is live`));
 
 module.exports = server;
