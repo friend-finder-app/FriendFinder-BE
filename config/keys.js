e66b761869e01e22687863606609bc8c70d8c12f
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-01 18:47:55 -0700

    updated mongo Node string

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: `mongodb+srv://diaz4674:Cc3058366@cluster0-pcqf4.gcp.mongodb.net/Friends?retryWrites=true&w=majority`
+  mongoURI: `mongodb://diaz4674:Cc3058366@cluster0-shard-00-00-pcqf4.gcp.mongodb.net:27017,cluster0-shard-00-01-pcqf4.gcp.mongodb.net:27017,cluster0-shard-00-02-pcqf4.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`
 };
